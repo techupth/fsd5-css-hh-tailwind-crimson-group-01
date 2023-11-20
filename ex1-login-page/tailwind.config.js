42b32d10be574064404220eef0640d9e6716a53a
bodin <bodinsonsawan@gmail.com>
2023-11-20 15:03:21 +0700

    feat(ex1)add styled login page

---
+++ ex1-login-page/tailwind.config.js
@@ -0,0 +1,25 @@
+const colors = require("tailwindcss/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        yellow: colors.yellow,
+        stone: colors.warmGray,
+        sky: colors.lightBlue,
+        neutral: colors.trueGray,
+        gray: colors.coolGray,
+        slate: colors.blueGray,
+      },
+    },
+  },
+  plugins: [],
+};
